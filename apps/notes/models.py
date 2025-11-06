261140db882a9b3952d5a2998cab81ed223a0244
anywaymy <simpletop4ik@gmail.com>
2025-11-06 10:41:58 +0300

    created app notes and serializers

---
+++ apps/notes/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.core.validators import MaxLengthValidator
+
+
+class Note(models.Model):
+    # Поля модели
+    title = models.CharField(max_length=200,
+                             verbose_name='Заголовок',
+                             help_text='Введите заголовок заметки') # verbose_name='Заголовок' для русификации в админке
+
+    content = models.TextField(validators=[MaxLengthValidator(10000)],
+                               verbose_name='Содержание',
+                               help_text='Введите содержание заметки')
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата создания'
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True, # автоматом обновится время после изменения
+        verbose_name = 'Дата обновления'
+    )
+
+    class Meta:
+        verbose_name = "Заметка"
+        verbose_name_plural = "Заметки"
+        ordering = ['-created_at']
+
+    def __str__(self):
+        # Возвращает представление объекта в виде строки
+        return self.title[:50]
